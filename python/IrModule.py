8193ee88a8e6c3b40fa27cadb4492e94963610e1
nphilli3 <nphilli3@gmail.com>
2016-01-11 01:12:57 -0500

    Here is a simple IR lisener in python

---
+++ python/IrModule.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+sensor = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+current = GPIO.input(sensor)
+previous = current
+def printState(current):
+    print 'GPIO pin %s is %s' % (sensor, 'HIGH' if current else 'LOW')
+printState(current)
+while True:
+    current = GPIO.input(sensor)
+    if current != previous:
+        printState(current)
+    previous = current
+    time.sleep(0.1)
+GPIO.cleanup()
