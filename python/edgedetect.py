28023df7786a1f029ecae22c0764732bb86d3fc5
Christopher <cruss1989@gmail.com>
2016-01-18 01:32:40 +0000

    changed to pin 4

--- python/edgedetect.py
+++ python/edgedetect.py
@@ -2,17 +2,17 @@ import RPi.GPIO as GPIO
 from time import sleep     # this lets us have a time delay (see line 12)  
   
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(4, GPIO.IN)    # set GPIO25 as input (button)  
+GPIO.setup(14, GPIO.IN)    # set GPIO25 as input (button)  
   
 def my_callback(channel):  
-    if GPIO.input(4):     # if port 4 == 1  
+    if GPIO.input(14):     # if port 4 == 1  
         print ("Rising edge detected on 4")  
     else:                  # if port 25 != 1  
         print ("Falling edge detected on 4")  
   
 # when a changing edge is detected on port 4, regardless of whatever   
 # else is happening in the program, the function my_callback will be run  
-GPIO.add_event_detect(4, GPIO.BOTH, callback=my_callback)  
+GPIO.add_event_detect(14, GPIO.BOTH, callback=my_callback)  
 
 
 #while True:
