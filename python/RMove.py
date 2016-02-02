c4211168f950500908225fc87b31f7c24eadd865
eudai <cruss1989@gmail.com>
2016-02-01 21:45:35 -0500

    added move library for py

--- python/RMove.py
+++ python/RMove.py
@@ -1,6 +1,6 @@
 import RPi GPIO as GPIO
 import time
-import picamera
+
 
 init(): #this sets up the pins and camera for movement and video
 	GPIO.setmode(GPIO.BCM)
@@ -11,62 +11,60 @@ init(): #this sets up the pins and camera for movement and video
 	GPIO.setup(25, GPIO.OUT)
 	GPIO.setup(18, GPIO.OUT)
 	GPIO.setup(12, GPIO.OUT)
-	p = GPIO.PWM(18, 80)
-	p1 = GPIO.PWM(12, 80)
-	camera = picamera.PiCamera()
-	camera.start_recording('/home/pi/Videos/test')
+	p = GPIO.PWM(18, 90)
+	p1 = GPIO.PWM(12, 90)
+	
 	GPIO.setup(2, GPIO.IN)    # set GPIO25 as input (button)
-	p_amt = 100      # left
-	p1_amt = 100     # right
+	
+	p_amt = 90      # left
+	p1_amt = 90     # right
 	
 def forward():
-	init()
-	GPIO.output(14, 0)
-	GPIO.output(15, 1)
-	GPIO.output(23, 0)
-	GPIO.output(24, 1)
+    GPIO.output(14, 0)
+    GPIO.output(15, 1)
+    GPIO.output(23, 0)
+    GPIO.output(24, 1)
 	
 def right_pivot():
-    init()
     GPIO.output(14, 1)
     GPIO.output(15, 0)
     GPIO.output(23, 0)
     GPIO.output(24, 1)
 
 def right_arch():
-    init()
     GPIO.output(14, 0)
     GPIO.output(15, 0)
     GPIO.output(23, 0)
     GPIO.output(24, 1)
     
 def left_arch():
-    init()
     GPIO.output(14, 0)
     GPIO.output(15, 1)
     GPIO.output(23, 0)
     GPIO.output(24, 0)
 
 def reverse():
-    p1.start(p1_amt)
-    p.start(p_amt)
     GPIO.output(14, 1)
     GPIO.output(15, 0)
     GPIO.output(23, 1)
     GPIO.output(24, 0)
 
 def left_pivot():
-    p1.start(p1_amt)
-    p.start(p_amt)
     GPIO.output(14, 0)
     GPIO.output(15, 1)
     GPIO.output(23, 1)
     GPIO.output(24, 0)
 
 def stop_moving():
-    p1.start(p1_amt)
-    p.start(p_amt)
     GPIO.output(14, 0)
     GPIO.output(15, 0)
     GPIO.output(23, 0)
     GPIO.output(24, 0)
+    
+def test_sequence():
+    seq = {'forward':forward,'reverse':reverse,'left_arch': left_arch,'right_arch':right_arch,'left_pivot': left_pivot,'right_pivot': right_pivot}
+    for i in seq:
+        seq[i]()
+        time.sleep(3)
+    stop_moving()
+    print ("test complete.")
--- python/Robot.py
+++ python/Robot.py
@@ -1,101 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 import picamera
+import RobotMethods
+import RMove *
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-
-p = GPIO.PWM(18, 80)
-p1 = GPIO.PWM(12, 80)
 
 def record():
-	
 	camera = picamera.PiCamera()
 	camera.start_recording('/home/pi/Videos/test')
-	time.sleep(30)
-	camera.stop_recording()
-#dr = input ("What direction?:")
-#tm = input ("How long?:")
-
-
-GPIO.setup(2, GPIO.IN)    # set GPIO25 as input (button)  
-
-p_amt = 100      # left
-p1_amt = 100     # right
-
-
-def forward():
-	p.start(p_amt)
-	p1.start(p1_amt)
-	GPIO.output(14, 0)
-	GPIO.output(15, 1)
-	GPIO.output(23, 0)
-	GPIO.output(24, 1)
-
-def right_pivot():
-    p1.start(p1_amt)
-    p.start(p_amt)
-    GPIO.output(14, 1)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 1)
-
-def right_arch():
-    p.start(p_amt)
-    GPIO.output(14, 0)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 1)
-    
-def left_arch():
-    p1.start(p1_amt)
-    p.start(p_amt)
-    GPIO.output(14, 0)
-    GPIO.output(15, 1)
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
-
-def reverse():
-    p1.start(p1_amt)
-    p.start(p_amt)
-    GPIO.output(14, 1)
-    GPIO.output(15, 0)
-    GPIO.output(23, 1)
-    GPIO.output(24, 0)
-
-def left_pivot():
-    p1.start(p1_amt)
-    p.start(p_amt)
-    GPIO.output(14, 0)
-    GPIO.output(15, 1)
-    GPIO.output(23, 1)
-    GPIO.output(24, 0)
-
-def stop_moving():
-    p1.start(p1_amt)
-    p.start(p_amt)
-    GPIO.output(14, 0)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
-
-    
-def test_sequence():
-    seq = {'forward':forward,'reverse':reverse,'left_arch': left_arch,'right_arch':right_arch,'left_pivot': left_pivot,'right_pivot': right_pivot}
-    for i in seq:
-        seq[i]()
-        time.sleep(3)
-    stop_moving()
-    print ("test complete.")
+	
 
 
 def edge_detected(channel):
+	record()
     pin_active = GPIO.input(channel)
     events.append(pin_active)
     if pin_active: # if port 2 == 1  
@@ -117,9 +34,8 @@ print('hello.')
 time.sleep(30)
 
 
-
-GPIO.cleanup()
+camera.stop_recording()
 print('here is what happened:')
 print(events)
 print('goodbye.')
-
+GPIO.cleanup()
