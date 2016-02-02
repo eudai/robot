fefc13b6baeac5393c93523f4a08ea9d252adfcd
eudai <cruss1989@gmail.com>
2016-02-02 01:34:23 -0500

    The INIT function does not work as a seperate file. all other functions work. Made this change. it functions now

--- python/RMove.py
+++ python/RMove.py
@@ -1,70 +1,74 @@
-import RPi GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
+class Motion:
+		
+	def init(self): #this sets up the pins for movement
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(14, GPIO.OUT) #Right motors backward
+		GPIO.setup(15, GPIO.OUT) #Right motors forward
+		GPIO.setup(23, GPIO.OUT) #Left motors backward
+		GPIO.setup(24, GPIO.OUT) #Left motors forward
+		GPIO.setup(25, GPIO.OUT)
+		GPIO.setup(18, GPIO.OUT)
+		GPIO.setup(12, GPIO.OUT)
+		
+		p = GPIO.PWM(18, 90)
+		p1 = GPIO.PWM(12, 90)
+		
+		GPIO.setup(2, GPIO.IN)    # set GPIO2 as input (button)
+		
+		p.start(90)      # left
+		p1.start(90)     # right
+		print("Motion initiallized")
+		
+	def forward(self):
+		GPIO.output(14, 0)
+		GPIO.output(15, 1)
+		GPIO.output(23, 0)
+		GPIO.output(24, 1)
+		print("moving forward")
+		
+	def right_pivot(self):
+		GPIO.output(14, 1)
+		GPIO.output(15, 0)
+		GPIO.output(23, 0)
+		GPIO.output(24, 1)
 
-init(): #this sets up the pins for movement
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(14, GPIO.OUT) #Right motors backward
-	GPIO.setup(15, GPIO.OUT) #Right motors forward
-	GPIO.setup(23, GPIO.OUT) #Left motors backward
-	GPIO.setup(24, GPIO.OUT) #Left motors forward
-	GPIO.setup(25, GPIO.OUT)
-	GPIO.setup(18, GPIO.OUT)
-	GPIO.setup(12, GPIO.OUT)
-	p = GPIO.PWM(18, 90)
-	p1 = GPIO.PWM(12, 90)
-	
-	GPIO.setup(2, GPIO.IN)    # set GPIO25 as input (button)
-	
-	p_amt = 90      # left
-	p1_amt = 90     # right
-	
-def forward():
-    GPIO.output(14, 0)
-    GPIO.output(15, 1)
-    GPIO.output(23, 0)
-    GPIO.output(24, 1)
-	
-def right_pivot():
-    GPIO.output(14, 1)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 1)
+	def right_arch(self):
+		GPIO.output(14, 0)
+		GPIO.output(15, 0)
+		GPIO.output(23, 0)
+		GPIO.output(24, 1)
+		
+	def left_arch(self):
+		GPIO.output(14, 0)
+		GPIO.output(15, 1)
+		GPIO.output(23, 0)
+		GPIO.output(24, 0)
 
-def right_arch():
-    GPIO.output(14, 0)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 1)
-    
-def left_arch():
-    GPIO.output(14, 0)
-    GPIO.output(15, 1)
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
+	def reverse(self):
+		GPIO.output(14, 1)
+		GPIO.output(15, 0)
+		GPIO.output(23, 1)
+		GPIO.output(24, 0)
 
-def reverse():
-    GPIO.output(14, 1)
-    GPIO.output(15, 0)
-    GPIO.output(23, 1)
-    GPIO.output(24, 0)
+	def left_pivot(self):
+		GPIO.output(14, 0)
+		GPIO.output(15, 1)
+		GPIO.output(23, 1)
+		GPIO.output(24, 0)
 
-def left_pivot():
-    GPIO.output(14, 0)
-    GPIO.output(15, 1)
-    GPIO.output(23, 1)
-    GPIO.output(24, 0)
-
-def stop_moving():
-    GPIO.output(14, 0)
-    GPIO.output(15, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
-    
-def test_sequence():
-    seq = {'forward':forward,'reverse':reverse,'left_arch': left_arch,'right_arch':right_arch,'left_pivot': left_pivot,'right_pivot': right_pivot}
-    for i in seq:
-        seq[i]()
-        time.sleep(3)
-    stop_moving()
-    print ("test complete.")
+	def stop_moving(self):
+		GPIO.output(14, 0)
+		GPIO.output(15, 0)
+		GPIO.output(23, 0)
+		GPIO.output(24, 0)
+		
+	def test_sequence(self):
+		seq = {'forward':forward,'reverse':reverse,'left_arch': left_arch,'right_arch':right_arch,'left_pivot': left_pivot,'right_pivot': right_pivot}
+		for i in seq:
+			seq[i]()
+			time.sleep(3)
+		stop_moving()
+		print ("test complete.")
--- python/Robot.py
+++ python/Robot.py
@@ -1,11 +1,32 @@
 import RPi.GPIO as GPIO
 import time
 import picamera
-import RobotMethods
-import RMove *
+from RMove import Motion
 
-init()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT) #Right motors backward
+GPIO.setup(15, GPIO.OUT) #Right motors forward
+GPIO.setup(23, GPIO.OUT) #Left motors backward
+GPIO.setup(24, GPIO.OUT) #Left motors forward
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
+p = GPIO.PWM(18, 90)
+p1 = GPIO.PWM(12, 90)
+
+GPIO.setup(2, GPIO.IN)    # set GPIO2 as input (button)
+
+p.start(90)      # left
+p1.start(90)     # right
+print("Motion initiallized")
+
+
+
+
+motion = Motion()
+
+#camera = picamera.PiCamera()
 def record():
 	camera = picamera.PiCamera()
 	camera.start_recording('/home/pi/Videos/test.h264')
@@ -13,17 +34,17 @@ def record():
 
 
 def edge_detected(channel):
-	record()
+ #   record()
     pin_active = GPIO.input(channel)
     events.append(pin_active)
     if pin_active: # if port 2 == 1  
         # print ("Rising edge detected on 2")
-        forward()
+        motion.forward()
     else:                  # if port 2 != 1  
         # print ("Falling edge detected on 2")
-        reverse()
+        motion.reverse()
         time.sleep(.3)
-        right_pivot()
+        motion.right_pivot()
         time.sleep(.5)
   
  #when a changing edge is detected on port 2, regardless of whatever   
@@ -35,7 +56,7 @@ print('hello.')
 time.sleep(30)
 
 
-camera.stop_recording()
+#camera.stop_recording()
 print('here is what happened:')
 print(events)
 print('goodbye.')
